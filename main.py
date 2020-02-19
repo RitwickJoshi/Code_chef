5a3f731de586512e26b7d97ee5805e60aa0aa383
RitwickJoshi <ritwickjoshi@gmail.com>
2020-02-19 07:43:20 +0000

    Did the challenge with correct answer

--- main.py
+++ main.py
@@ -13,12 +13,25 @@ def sub_lists(list1,n):
 
 
 t = int(input())
-if t<=10:
-    k,n = list(map(int,input().split()))
+final_answer=[]
+for _ in range(t):
+    if t<=10:
+        k,n = list(map(int,input().split()))
 
-    
-    A=list(map(int,input().split()))
+        
+        A=list(map(int,input().split()))
 
-    sub_sequence = sub_lists(A,n)
+        sub_sequence = sub_lists(A,n)
 
-print(sub_sequence)
+    sum_list=[]
+    add=0
+    for item in range(1,len(sub_sequence)):
+        add = 0
+        for each in sub_sequence[item]:
+            add+=each
+        sum_list.append(add)
+    final_answer.append(sum_list.index(min(sum_list))+1)
+    sum_list.clear()
+
+for each_index in final_answer:
+    print(each_index)
